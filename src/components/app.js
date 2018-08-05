834de32f7d121f7bd46f564f28e12773c22c8cc2
Jozue06 <jozue06@gmail.com>
2018-08-04 21:10:09 -0700

    init

---
+++ src/components/app.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import counterAppReducer from '../reducers/counter-app';
+const store = createStore(counterAppReducer);
+
+import Dashboard from './Dashboard';
+
+class App extends React.Component {
+  render() {
+    return (
+			<Provider store={store}>
+				<BrowserRouter>
+					<Route exact path='/' component={Dashboard}/>
+				</BrowserRouter>
+			</Provider>
+    )
+  }
+}
+
+export default App
